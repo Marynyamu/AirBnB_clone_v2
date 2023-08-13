be3549cbba35b4073e75668361e93b841a90a512
wanjanyamu01@gmail.com <liznyaporo@gmail.com>
2023-08-13 17:15:33 +0300

    AirBnB

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,20 +35,17 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary of objects"""
-        return type(self).__objects
+
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets new obj in __objects dictionary."""
-        if obj.id in type(self).__objects:
-            print("exists")
-            return
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        type(self).__objects[key] = obj
-        # OR
-        # type(self).__objects[obj.id] = obj
+
+        FileStorage.__objects[f'{obj.__class__.__name__}.{obj.id}'] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
+
         new_dict = []
         for obj in type(self).__objects.values():
             new_dict.append(obj.to_dict())
@@ -56,19 +53,14 @@ class FileStorage:
             #    new_dict[key] = obj.to_dict()
         with open(type(self).__file_path, "w", encoding='utf-8') as file:
             json.dump(new_dict, file)
-            # OR
-            # with open(type(self).__file_path, "w", encoding="utf-8") as file:
-            #   json.dump([obj.to_dict() for obj in self.all().values()], file)
+
 
     def reload(self):
         """Deserializes the JSON file to __objects if it exists"""
-        if os.path.exists(type(self).__file_path) is True:
+        try:
+            with open(FileStorage.__file_path) as file:
+                dict_from_json = load(file)
+                for obj in dict_from_json.values():
+                    self.new(eval(obj['__class__'])(**obj))
+        except Exception:
             return
-            try:
-                with open(type(self).__file_path, "r") as file:
-                    new_obj = json.load(file)
-                    for key, val in new_obj.items():
-                        obj = self.class_dict[val['__class__']](**val)
-                        type(self).__objects[key] = obj
-            except Exception:
-                pass
