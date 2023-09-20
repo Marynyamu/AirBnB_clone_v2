d4c4ed83769f701146e534b63b10594341de4016
wanjanyamu01@gmail.com <wanjanyamu01@gmail.com>
2023-09-20 07:33:37 -0700

    AirBnB2

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""__init__ method for models package, or
-Module for FileStorage autoinit.
-"""
+"""This module instantiates an instance of the Storage will be used"""
 
-from models.engine.file_storage import FileStorage
+from os import getenv
 
-storage = FileStorage()
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
 storage.reload()
