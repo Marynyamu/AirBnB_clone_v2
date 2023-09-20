22f8ce1214ac008af2cc7ee5c30d3518c4854fb4
wanjanyamu01@gmail.com <wanjanyamu01@gmail.com>
2023-09-20 07:09:11 -0700

    models

--- models/review.py
+++ models/review.py
@@ -1,12 +1,17 @@
-#!/bin/usr/bin/python3
-"""The module inherit from BaseModel"""
+#!/usr/bin/python3
+""" Review module for the HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.sql.schema import ForeignKey
+from os import getenv
 
-from models.base_model import BaseModel
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class Review(BaseModel):
-    """Is the class for user reviews"""
-
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review classto store review information """
+    if storage_type == 'db':
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(String(1024), nullable=False)
